7c9418fbd163758e226d5ab99f5aef1cd55c29ee
programa-jota <jjcg98@gmail.com>
2022-11-18 22:25:31 -0500

    Creacion del controlador para Plan

--- Backend/BackendPedidos/src/controllers/index.ts
+++ Backend/BackendPedidos/src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './mascota-usuario.controller';
 export * from './mascota-plan.controller';
 export * from './usuario.controller';
 export * from './mascota.controller';
+export * from './plan.controller';
