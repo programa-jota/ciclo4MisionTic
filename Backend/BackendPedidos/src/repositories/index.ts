951c9da4bc8405749a31a651b9f21bf62ab8e08e
programa-jota <jjcg98@gmail.com>
2022-11-18 22:07:57 -0500

    Creacion de los repositorios de todos los modelos

---
+++ Backend/BackendPedidos/src/repositories/index.ts
@@ -0,0 +1,6 @@
+export * from './mascota.repository';
+export * from './plan.repository';
+export * from './producto.repository';
+export * from './prospecto.repository';
+export * from './sucursal.repository';
+export * from './usuario.repository';
