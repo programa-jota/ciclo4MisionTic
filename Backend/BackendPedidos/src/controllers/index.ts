62c831451af4161d7088206bc1838f4f9ae0367e
programa-jota <jjcg98@gmail.com>
2022-11-18 22:24:47 -0500

    Creacion del controlador para Mascota

--- Backend/BackendPedidos/src/controllers/index.ts
+++ Backend/BackendPedidos/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './mascota-usuario.controller';
 export * from './mascota-plan.controller';
 export * from './usuario.controller';
+export * from './mascota.controller';
